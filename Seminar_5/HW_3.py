71caef9906e467c9661e13fd4e5eda27ca46f61d
A.Krivolapov <akrivola@ya.ru>
2023-11-07 12:18:53 +0300

    HW 5 3

---
+++ Seminar_5/HW_3.py
@@ -0,0 +1,16 @@
+'''
+Создайте функцию генератор чисел Фибоначчи fibonacci.
+'''
+
+
+def fibonacci():
+    a = 0
+    b = 1
+    while (True):
+        yield a
+        a, b = b, a + b
+
+
+f = fibonacci()
+for i in range(10):
+    print(next(f))
