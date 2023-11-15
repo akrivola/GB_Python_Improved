29f5db9e649a77187d0cc91600165d6e30c4adeb
A.Krivolapov <akrivola@ya.ru>
2023-11-15 12:15:34 +0300

    HW 6 3

--- Seminar_6/HW_3_2.py
+++ Seminar_6/HW_3_2.py
@@ -16,17 +16,23 @@ def generate_boards():
     while len(board_list) < 4:
         board = []
         i = 0
+        overload = False
         while len(board) < 8:
             i += 1
             x = randint(1, 8)
             y = randint(1, 8)
             if all(x != c[0] and y != c[1] and abs(x - c[0]) != abs(y - c[1]) for c in board):
                 board.append((x, y))
-            if i > 100:
+            if i > 1000:
+                overload = True
                 break
 
-        board_list.append(board)
+        if not overload and not board in board_list:
+            board_list.append(board)
     return board_list
+
 
 board_list = generate_boards()
 print(board_list)
+for i in board_list:
+    print(len(i), i)
