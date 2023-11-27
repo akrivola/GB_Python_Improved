5b56b101b550251fe4228a41f05a954b663a35d6
A.Krivolapov <akrivola@ya.ru>
2023-11-27 13:44:22 +0300

    HW 8 1

--- Seminar_8/HW_1/HW_1.py
+++ Seminar_8/HW_1/HW_1.py
@@ -5,33 +5,47 @@ import pickle
 from pathlib import Path
 
 
-def traverse_directory(directory: str, total_dct: dict = None) -> dict[str, dict[str]]:
-    if total_dct is None:
-        total_dct = {}
-        basic_path = os.path.split(os.path.abspath(directory))                                           #('C:\\Users\\Алексей\\Desktop\\GreekBrains\\СЕМИНАРЫ\\Python_advanced', 'Seminar_8')
+def traverse_directory(directory: str, total_lst: list = None, is_branch: bool = False):
+    if total_lst is None:
+        total_lst = []
+        #basic_path = os.path.split(os.path.abspath(directory))
 
-        dct_formatter1(total_dct, os.path.join(*basic_path[:-1]), basic_path[-1], 'D')
-
+        #lst_formatter(total_lst, os.path.abspath(directory), os.path.join(*basic_path[:-1]), basic_path[-1], 'D')
 
     for item in os.listdir(directory):
         check_path = os.path.join(directory, item)
         if os.path.isfile(check_path):
-            dct_formatter1(total_dct, directory, item, 'F')
+#            lst_formatter(total_lst, check_path, directory, item, 'F')
+            total_lst.append(dict(Path=check_path,
+                                  Type='File',
+                                  # object_name=item_name,
+                                  Size=os.path.getsize(os.path.join(directory, item)),
+                                  parent_directory=os.path.split(directory)[-1]))
+
         elif os.path.isdir(check_path):
-            dct_formatter1(total_dct, directory, item, 'D')
-            traverse_directory(os.path.join(directory, item), total_dct)
-    return total_dct
-def dct_formatter1(total_dct: dict[str, dict[str]], path: str, item_name: str, item_type: str) -> None:
+#            lst_formatter(total_lst, check_path, directory, item, 'D')
+            total_lst.append(dict(Path=check_path,
+                                  Type='Directory',
+                                  # object_name=item_name,
+                                  Size=count_size(os.path.join(directory, item)),
+                                  parent_directory=os.path.split(os.path.abspath(directory))[-1]))
+            traverse_directory(os.path.join(directory, item), total_lst, True)
+    return total_lst
+
+
+def lst_formatter(total_lst: list, full_path: str, path: str, item_name: str, item_type: str) -> None:
     if item_type == 'F':
-        total_dct[path] = dict(object_type='File',
-                               object_name=item_name,
-                               object_size=os.path.getsize(os.path.join(path, item_name)),
-                               parant_directory=os.path.split(path)[-1])
+        total_lst.append(dict(Path=full_path,
+                              Type='File',
+                              #object_name=item_name,
+                              Size=os.path.getsize(os.path.join(path, item_name)),
+                              parent_directory=os.path.split(path)[-1]))
     elif item_type == 'D':
-        total_dct[path] = dict(object_type='Directory',
-                               object_name=item_name,
-                               object_size=count_size(os.path.join(path, item_name)),
-                               parant_directory=os.path.split(os.path.abspath(path))[-1])
+        total_lst.append(dict(Path=full_path,
+                              Type='Directory',
+                              #object_name=item_name,
+                              Size=count_size(os.path.join(path, item_name)),
+                              parent_directory=os.path.split(os.path.abspath(path))[-1]))
     else:
         pass
 
@@ -40,25 +54,29 @@ def count_size(count_path: str, dir_size: int = 0) -> float:
     for sub_item in os.walk(count_path):
 
         if sub_item[2]:
-            dir_size += sum([os.path.getsize(os.path.join(sub_item[0], file))for file in sub_item[2]])  # размер всех файлов в директории
+            dir_size += sum([os.path.getsize(os.path.join(sub_item[0], file)) for file in
+                             sub_item[2]])  # размер всех файлов в директории
 
         if sub_item[1]:
-            dir_size += sum([count_size(os.path.join(sub_item[0], subdir))for subdir in sub_item[1]])
+            dir_size += sum([count_size(os.path.join(sub_item[0], subdir)) for subdir in sub_item[1]])
 
     return dir_size
 
 
-def save_results_to_json(data_dict: dict, path: str, file_name: str) -> None:                             #запись словаря в json файл
+def save_results_to_json(data: list, path: str, file_name: str) -> None:  # запись словаря в json файл
     file_path = os.path.join(path, file_name + '.json')
     with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(data_dict, f, indent=4)
+        json.dump(data, f, indent=4)
 
 
-def save_results_to_csv(data_dict: dict, path: str, file_name: str) -> None:                             #запись словаря в csv файл
+def save_results_to_csv(data_lst: list, path: str, file_name: str) -> None:  # запись словаря в csv файл
     file_path = os.path.join(path, file_name + '.csv')
-    data = [['Path', 'object_type',  'object_name', 'object_size', 'parant_directory']]
-    for key, value in data_dict.items():
-        data.append([key, *value.values()])
+    data = [['Path', 'object_type', 'object_name', 'object_size', 'parent_directory']]
+    for _ in data_lst:
+        new_lst = []
+        for key, item in _.items():
+            new_lst.append(item)
+        data.append(new_lst)
     #print(data)
 
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -66,15 +84,13 @@ def save_results_to_csv(data_dict: dict, path: str, file_name: str) -> None:    
         write_csv.writerows(data)
 
 
-def save_results_to_pickle(data_dict: dict, path: str, file_name: str) -> None:                                #запись словаря в pickle файл
+def save_results_to_pickle(data: list, path: str, file_name: str) -> None:  # запись словаря в pickle файл
     file_path = os.path.join(path, file_name + '.pickle')
     with open(file_path, 'wb') as f:
-        pickle.dump(data_dict, f)
+        pickle.dump(data, f)
 
-
-
-result = traverse_directory(directory=str(Path.cwd()) + '\\directory')
-print(result)
-#save_results_to_json(result, os.getcwd(), 'result')
-#save_results_to_csv(result, os.getcwd(), 'result')
-#save_results_to_pickle(result, os.getcwd(), 'result')
+results = traverse_directory(directory=str(Path.cwd()) + '\\directory')
+print(results)
+save_results_to_json(results, os.getcwd(), 'result')
+save_results_to_csv(results, os.getcwd(), 'result')
+save_results_to_pickle(results, os.getcwd(), 'result')
