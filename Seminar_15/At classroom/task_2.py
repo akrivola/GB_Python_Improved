26ffca7d1fda0cd713d2907a63244498fc79f5f9
akrivola <akrivola@ya.ru>
2024-01-29 22:39:31 +0300

    Семинар

---
+++ Seminar_15/At classroom/task_2.py
@@ -0,0 +1,28 @@
+import logging
+
+logging.basicConfig(filename='Log/log_2.log',
+                    filemode='w',
+                    encoding='utf-8',
+                    format='{levelname} - {asctime} в строке '
+                           '{lineno} функция "{funcName}()" : {msg}',
+                    style='{',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        logger.info(f'аргументы функции: {args}, результат функции: {result}')
+        return result
+
+    return wrapper
+
+
+@decor
+def power(x, y):
+    return x ** y
+
+
+print(power(2, 10))
+print(power(5, 3))
